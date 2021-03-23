787417c414402d20410aa58ec558be1cfb4738b5
qayyamj <qayyamj@gmail.com>
2021-03-23 09:50:11 -0400

    restructured files

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 app.use('/', indexRouter);
 app.use(function (req, res, next) {
     next(createError(404));
--- app.ts
+++ app.ts
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'Public')));
+app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', indexRouter);
 
