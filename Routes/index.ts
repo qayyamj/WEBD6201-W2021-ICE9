92ac949137fe429237207fbede96471ae7322c6b
qayyamj <qayyamj@gmail.com>
2021-03-24 15:30:45 -0400

    added navbar links

--- Routes/index.ts
+++ Routes/index.ts
@@ -5,14 +5,61 @@ const router = express.Router();
 router.get('/', function(req, res, next) 
 {
   // use the 'index' template and inject an object with a 'title' property with a val of 'Express'
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home', page: 'home'});
 });
 
 /* GET home page = with /home */
 router.get('/home', function(req, res, next) 
 {
   // use the 'index' template and inject an object with a 'title' property with a val of 'Express'
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' , page: 'home'});
 });
+
+/* GET about page  = with /about */
+router.get('/about', function(req, res, next) 
+{
+  // use the 'index' template and inject an object with a 'title' property with a val of 'Express'
+  res.render('index', { title: 'About' , page: 'about'});
+});
+
+
+/* GET services page = with /services */
+router.get('/services', function(req, res, next) 
+{
+  // use the 'index' template and inject an object with a 'title' property with a val of 'Express'
+  res.render('index', { title: 'Services' , page: 'services'});
+});
+
+
+/* GET project page = with /projects */
+router.get('/projects', function(req, res, next) 
+{
+  // use the 'index' template and inject an object with a 'title' property with a val of 'Express'
+  res.render('index', { title: 'Projects' , page: 'projects'});
+});
+
+/* GET contact page = with /contact */
+router.get('/contact', function(req, res, next) 
+{
+  // use the 'index' template and inject an object with a 'title' property with a val of 'Express'
+  res.render('index', { title: 'Contact Us', page: 'contact' });
+});
+
+/* GET login page = with /login */
+router.get('/login', function(req, res, next) 
+{
+  // use the 'index' template and inject an object with a 'title' property with a val of 'Express'
+  res.render('index', { title: 'Login', page: 'login' });
+});
+
+
+/* GET register page = with /register */
+router.get('/register', function(req, res, next) 
+{
+  // use the 'index' template and inject an object with a 'title' property with a val of 'Express'
+  res.render('index', { title: 'Register', page: 'register' });
+});
+
+
 
 module.exports = router;
--- app.ts
+++ app.ts
@@ -33,9 +33,12 @@ app.use(function(err:createError.HttpError, req:express.Request, res:express.Res
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
+  let message = err.message;
+  let error = req.app.get('env') === 'development' ? err : {};
+
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {message: message, error:error});
 });
 
 module.exports = app;
